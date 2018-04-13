918ef4b30a71ec11622447222f22e6b643836f1c
AurimasVilys <aurimas.vilys.96@gmail.com>
2018-04-13 08:47:57 +0300

    Antros  užd failai

---
+++ 2uzduotis/README.md
@@ -0,0 +1,28 @@
+# Ticket Stats
+
+### Running
+You need to have docker and docker-compose installed.
+To build and run:
+
+1. docker-compose build
+
+2. docker-compose up -d
+
+Used ports are 5000 and 81
+
+
+### Queries 1FS TASK:
+1. [GET] Get all tickets -> http://localhost:5000/tickets
+2. [GET] Get single ticket info -> http://localhost:5000/tickets/<Ticket_ID>
+2. [GET] Get all tickets for specific event -> http://localhost:5000/events/<Event_Id>/tickets
+3. [PATCH] Generate barcode -> http://localhost:5000/tickets/<Ticket_id>
+4. [PATCH] Generate all tickets barcode for specific event -> http://localhost:5000/events/<Event_id>/tickets
+5. [PUT] Change ticket info (Only "Event No" and "Current zone". Parameter Passed by JSON ("Event No": string, "Current Zone": string)) -> http://localhost:5000/tickets/<ticketID>
+6. [POST] Add ticket to the event -> http://localhost:5000/tickets (Parameters passed by JSON. "Event No": string)
+7. [DELETE] Remove ticket http://localhost:5000/tickets/<ticketID>
+
+### Queries 2ND TASK:
+1. [GET] Get all events -> http://localhost:5000/events
+1. [POST] Create tickets for event -> http://localhost:5000/events/tickets
+1. [PATCH] Rate film -> http://localhost:5000/events/rate
+
---
+++ 2uzduotis/tickets_stat.py
@@ -0,0 +1,167 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import abort
+from faker import Faker
+
+import requests
+import json
+import os
+import random
+
+app = Flask(__name__)
+
+ticketsDB = [
+	{'id' : '1', 'Barcode' : '', 'Event No' : '3', 'Current Zone' : '0', 'Rated': '0'},
+	{'id' : '2', 'Barcode' : '', 'Event No' : '5', 'Current Zone' : '0', 'Rated': '0'},
+	{'id' : '3', 'Barcode' : '', 'Event No' : '1', 'Current Zone' : '0', 'Rated': '0'},
+	{'id' : '4', 'Barcode' : '', 'Event No' : '6', 'Current Zone' : '0', 'Rated': '0'},
+	{'id' : '5', 'Barcode' : '', 'Event No' : '3', 'Current Zone' : '0', 'Rated': '0'},
+	{'id' : '6', 'Barcode' : '', 'Event No' : '2', 'Current Zone' : '0', 'Rated': '0'},
+	{'id' : '7', 'Barcode' : '', 'Event No' : '2', 'Current Zone' : '0', 'Rated': '0'}
+]
+
+@app.route('/')
+def hello():
+	return 'Connected'
+
+# 1ST TASK
+
+# GET information about current Tickets in JSON format
+@app.route('/tickets', methods=['GET'])
+def getCurrentTickets():
+	return jsonify(ticketsDB)
+
+# GET information about single Ticket in JSON format
+@app.route('/tickets/<ticketID>', methods=['GET'])
+def getSingleTicket(ticketID):
+	ticket = [tic for tic in ticketsDB if (tic['id'] == ticketID)]
+	return jsonify(ticket[0])
+
+# GET information about current Event Tickets in JSON format
+@app.route('/events/<eventID>/tickets', methods=['GET'])
+def getCurrentTicketsByEvent(eventID):
+	eventTickets = [ tic for tic in ticketsDB if (tic['Event No'] == eventID)]
+	return jsonify(eventTickets)
+
+
+# PUT a barcode to the specific Ticket ID. Id provided by URL
+@app.route('/tickets/<ticketID>', methods=['PATCH'])
+def generateTicket(ticketID):
+	randomBarcode = random.randint(10000000,100000000)
+	ticket = [tic for tic in ticketsDB if (tic['id'] == ticketID)]
+	if(ticket[0]['Barcode'] == ''):
+		ticket[0]['Barcode']  = randomBarcode
+		return jsonify(ticket[0])
+	else:
+		return 'Error. Ticket has a barcode generated', 405
+
+# PUT a barcode to specific Event Tickets. Event Id provided by URL
+@app.route('/events/<eventID>/tickets', methods=['PATCH'])
+def generateEventTicket(eventID):
+	ticket = [tic for tic in ticketsDB if (tic['Event No'] == eventID)]
+	for eventsTic in ticket:
+		if(eventsTic['Barcode'] == ''):
+			randomBarcode = random.randint(100000,1000000)
+			eventsTic['Barcode']  = randomBarcode
+	return jsonify(ticket)
+
+#PUT update info (only Event No)
+@app.route('/tickets/<ticketID>', methods=['PUT'])
+def changeEvent(ticketID):
+	ticket = [tic for tic in ticketsDB if (tic['id'] == ticketID)]
+	if (len(ticket) == 0):
+		abort(404)
+	if 'Event No' in request.json:
+		ticket[0]['Event No'] = request.json['Event No']
+	if 'Current Zone' in request.json:
+		ticket[0]['Current Zone'] = request.json['Current Zone']
+	if 'Barcode' in request.json:
+		ticket[0]['Barcode'] = request.json['Barcode']
+	return jsonify(ticket[0])
+
+# POST - Add one Ticket to the Event. Event ID passed by JSON. Ticket ID is auto increasing.
+@app.route('/tickets', methods = ['POST'])
+def addTicket():
+	lastId = int(ticketsDB[len(ticketsDB) - 1]['id']) + 1
+	ticket = {
+		'id': str(lastId),
+		'Barcode': '',
+		'Event No' :request.json['Event No'],
+		'Current Zone': '0',
+		'Rated': '0'
+	}
+	ticketsDB.append(ticket)
+	return jsonify(ticket), 201
+
+# DELETE - Remove ticket from the list.
+# Only when the barcode is not generated
+@app.route('/event/<ticketID>', methods = ['DELETE'])
+def deleteTicket(ticketID):
+	ticket = [tic for tic in ticketsDB if (tic['id'] == ticketID)]
+	if (len(ticket) == 0):
+		abort(404)
+	if (ticket[0]['Barcode'] == ''):
+		ticketsDB.remove(ticket[0])
+		return jsonify(ticket[0]), 200
+	else:
+		return 'Error. Ticket has a barcode generated and cannot be deleted', 405
+
+#2nd TASK
+
+# GET info about all events(movies)
+@app.route('/events', methods=['GET'])
+def getEvents():
+	r = requests.get('http://172.18.0.1:81/movies')
+	return r.text
+
+# POST - Add selected amount of tickets to the selected event. 
+#Amount of tickets, event(film) id passed by JSON. String and int
+@app.route('/events/tickets', methods = ['POST'])
+def addTicketToEvent():
+	filmTitle = request.json['Title']
+	if(len(filmTitle) == 0):
+		return 'Error. Title not provided or is invalid', 405
+	numberOfTickets = request.json['TicNumber']
+	if(numberOfTickets < 1):
+		return 'Error. TicNumber not provided or is invalid', 405
+	requestData = {'title' : filmTitle}
+	r = requests.get('http://172.18.0.1:81/movies', params=requestData)
+	eventID = r.json()
+	for i in range(0,numberOfTickets):
+		lastId = int(ticketsDB[len(ticketsDB) - 1]['id']) + 1
+		ticket = {
+			'id': str(lastId),
+			'Barcode': '',
+			'Event No' : eventID[0]['ID'],
+			'Current Zone': '0',
+			'Rated': '0'
+		}
+		ticketsDB.append(ticket)
+	eventTickets = [ tic for tic in ticketsDB if (tic['Event No'] == str(eventID[0]['ID']))]
+	return jsonify(eventTickets), 201
+
+# POST - Add selected amount of tickets to the selected event. 
+#Ticket ID, Ratinf id passed by JSON. Both String
+@app.route('/events/rate', methods = ['PATCH'])
+def addRatingToEvent():
+	ticketID = request.json['Ticket ID']
+	ticket = [tic for tic in ticketsDB if (tic['id'] == ticketID)]
+	if(len(ticketID) == 0):
+		return 'Error. TicketID not provided or is invalid', 405
+	if(ticket[0]['Rated'] == "1"):
+		return 'Error. Ticket already participated in rating', 405
+	data = {
+		"Rating": request.json['Rating']
+	}
+	print (data['Rating'])
+	url = 'http://172.18.0.1:81/movies'
+	r = requests.patch('{}/{}'.format(url, ticket[0]['Event No']), json=data)
+	if(r.status_code==200):
+		ticket[0]['Rated'] = "1"
+		return r.text, 200
+	return r.text, 404
+
+
+if __name__ == "__main__":
+	app.run(debug=True, host='0.0.0.0', threaded=True)
